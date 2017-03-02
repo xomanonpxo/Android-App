070b1473e46277d9a610583f10ba77fab5415366
Manon Philippot <manon.philippot@gmail.com>
2017-03-02 13:31:08 +0100

    Implémentation accès gallerie

--- app/src/main/java/com/example/xomanonpxo/android_app/MainActivity.java
+++ app/src/main/java/com/example/xomanonpxo/android_app/MainActivity.java
@@ -1,13 +1,48 @@
 package com.example.xomanonpxo.android_app;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int CAPTURE_IMAGE_FULLSIZE_ACTIVITY_REQUEST_CODE = 1777;
+    private static int RESULT_LOAD_IMG = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView photoView = (ImageView) findViewById(R.id.photoView);
+        ImageView galleryView = (ImageView) findViewById(R.id.galleryView);
+
+        photoView.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), SecondActivity.class);
+                int choice = 0;
+                intent.putExtra("choice", choice);
+                startActivity(intent);
+            }
+        });
+
+        galleryView.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), SecondActivity.class);
+                int choice = 1;
+                intent.putExtra("choice", choice);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/xomanonpxo/android_app/SecondActivity.java
+++ app/src/main/java/com/example/xomanonpxo/android_app/SecondActivity.java
@@ -2,11 +2,33 @@ package com.example.xomanonpxo.android_app;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+
+import static android.R.attr.data;
 
 public class SecondActivity extends AppCompatActivity {
+
+    public static final int REQUEST_CAMERA = 0;
+    public static final int SELECT_FILE = 1;
 
     ImageView imageView;
 
@@ -14,9 +36,85 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        selectImage();
+
         imageView = (ImageView)findViewById(R.id.imageView);
+    }
+
+    private void selectImage(){
         Intent intent = getIntent();
-        Bitmap bitmap = (Bitmap) intent.getParcelableExtra("BitmapImage");
+        int choice = (int)intent.getIntExtra("choice", -1);
+
+        if(choice == 0){
+            cameraIntent();
+        }
+        if(choice == 1){
+            galleryIntent();
+        }
+    }
+
+    private void cameraIntent(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File file = new File(Environment.getExternalStorageDirectory()+File.separator+"image.jpg");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+        startActivityForResult(intent, REQUEST_CAMERA);
+    }
+
+    private void galleryIntent(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select File"), SELECT_FILE);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK){
+            if (requestCode == REQUEST_CAMERA){
+                onCaptureImageResult(data);
+            }
+            else if (requestCode == SELECT_FILE){
+                onSelectFromGalleryResult(data);
+            }
+        }
+        else {
+            Toast.makeText(this, "You haven't picked any image.", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    private void onCaptureImageResult(Intent data) {
+        String path = Environment.getExternalStorageDirectory()+File.separator+"image.jpg";
+        File imgFile = new File(path);
+        if(imgFile.exists())
+        {
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+            Bitmap bitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath(), options);
+            imageView.setImageBitmap(bitmap);
+        }
+    }
+
+    private void onSelectFromGalleryResult(Intent data){
+        Bitmap bitmap = null;
+        if (data != null) {
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), data.getData());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         imageView.setImageBitmap(bitmap);
     }
 }
+
+
+/*
+TextView textView = (TextView)findViewById(R.id.textView);
+textView.setText("camera");
+
+public static final int CAPTURE_IMAGE_FULLSIZE_ACTIVITY_REQUEST_CODE = 1777;
+
+ */
